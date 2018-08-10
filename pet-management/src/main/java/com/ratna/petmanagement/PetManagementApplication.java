f6824dd38978fc7e3bc1482890d2e12ae3a6cba7
archanzels <archanzels@gmail.com>
2018-08-10 11:05:08 +0545

    Server Added

---
+++ pet-management/src/main/java/com/ratna/petmanagement/PetManagementApplication.java
@@ -0,0 +1,12 @@
+package com.ratna.petmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetManagementApplication.class, args);
+	}
+}
